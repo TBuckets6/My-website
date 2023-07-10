8e4a5d1425a26d8eaee3be559322a679dcd61197
Taylor Bushey <tbushey236@gmail.com>
2023-07-10 03:39:47 -0400

    another way to do the same thing as previous commit

--- calCal.js
+++ calCal.js
@@ -1,17 +1,20 @@
 
 
-const txtInput = document.getElementById('heightFt');
+// const txtInput = document.getElementById('heightFt');
 
-txtInput.style.backgroundColor = 'red'
+// txtInput.addEventListener('keyup', () => {
+//     //console.log('hi')
+//     //let y = document.getElementById('textOut')
+//     //y.innerHTML = txtInput.value
+//     let z = txtInput.value
+//     console.log(z)
+// })
+
+//let x = txtInput.value;
+
+const txtInput = document.getElementById('heightFt')
 
 txtInput.addEventListener('keyup', () => {
-    console.log('hi')
-    let y = document.getElementById('textOut')
-    y.innerHTML = txtInput.value
-    let z = txtInput.value
-    console.log(z)
+    console.log(txtInput.value)
 })
-
-let x = txtInput.value;
-
 
