d7b7ee2769d1bb68242df5efb0d579109680ac30
Taylor Bushey <tbushey236@gmail.com>
2023-12-21 17:21:00 -0500

    added contact section. need to test

--- contactEmail.js
+++ contactEmail.js
@@ -1,4 +1,20 @@
+const email = document.getElementById('email')
+const name = document.getElementById('name')
+const phone = document.getElementById('tel')
+const subject = document.getElementById('subject')
+const message = document.getElementById('message')
+const submit = document.getElementsByClassName('form-contact')[0]
 
-function sendEmail(){
-    
-}
+submit.addEventListener('submit', (e) => {
+  e.preventDefault()
+  
+  Email.send({
+    SecureToken: "f1b49bfd-531e-45ea-a31d-962c043ae1d3",
+    To: 'tbushey236@gmail.com',
+    From: email.value,
+    Subject: subject.value,
+    Body: message.value
+  }).then(
+    message => alert(message)
+  );
+})
