56048c5c5b195b13400565fdcf191c361903337c
Taylor Bushey <tbushey236@gmail.com>
2023-12-21 18:41:12 -0500

    contact section now functional

--- contactEmail.js
+++ contactEmail.js
@@ -1,19 +1,33 @@
 const email = document.getElementById('email')
-const name = document.getElementById('name')
-const phone = document.getElementById('tel')
+const namee = document.getElementById('namee')
+const tel = document.getElementById('tel')
 const subject = document.getElementById('subject')
 const message = document.getElementById('message')
 const submit = document.getElementsByClassName('form-contact')[0]
 
+//const body = 'Name: ' + namee.value + '<br>' + 'Email: ' + email.value + '<br>' + 'Phone: ' + tel.value + '<br>' + 'Content: ' + message.value
+
+
+
 submit.addEventListener('submit', (e) => {
   e.preventDefault()
-  
+
+  const body = `
+  Name: ${namee.value}
+  <br>
+  Email: ${email.value}
+  <br>
+  Phone: ${tel.value} 
+  <br>
+  Content: ${message.value}
+`
+
   Email.send({
     SecureToken: "f1b49bfd-531e-45ea-a31d-962c043ae1d3",
     To: 'tbushey236@gmail.com',
-    From: email.value,
+    From: 'tbushey236@gmail.com',
     Subject: subject.value,
-    Body: message.value
+    Body: body
   }).then(
     message => alert(message)
   );
