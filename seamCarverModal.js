83acc11a285a5a377fe002a62ab38718ac4c1380
Taylor Bushey <tbushey236@gmail.com>
2024-01-23 07:28:34 -0500

    added modal to weather app in projects

--- seamCarverModal.js
+++ seamCarverModal.js
@@ -1,14 +1,28 @@
 //This code is for the seam carving dialog box.
 
 const seamCarverImg = document.getElementById('seamCarverImg')
-const closeModal = document.getElementById('closeModal')
-const modal = document.getElementById('modalId')
+const weatherImg = document.getElementById('weatherImg')
+
+const closeSeamModal = document.getElementById('close-seam-modal')
+const closeWeatherModal = document.getElementById('close-weather-modal')
+
+const seamModal = document.getElementById('seamcarver-modal')//modal
+const weatherModal = document.getElementById('weather-modal')
 
 seamCarverImg.addEventListener('click', () => {
-    modal.showModal()
-    modal.scrollTop = 0
+    seamModal.showModal()
+    seamModal.scrollTop = 0
 })
 
-closeModal.addEventListener('click', () => {
-    modal.close()
+closeSeamModal.addEventListener('click', () => {
+    seamModal.close()
+})
+
+weatherImg.addEventListener('click', () => {
+    weatherModal.showModal()
+    weatherModal.scrollTop = 0
+})
+
+closeWeatherModal.addEventListener('click', () => {
+    weatherModal.close()
 })
